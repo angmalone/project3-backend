de2c5ea9049fe837c334efa3c45b1d80e5186323
Angela Malone <a.malone1193@gmail.com>
2018-03-27 16:48:40 -0400

    push request functionality

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const methodOverride = require("method-override");
 const Job = require("./db/jobs.js");
+const Company = require("./db/companies.js");
 
 const app = express();
 //app.use(express.static(path.join(__dirname, "public")));
@@ -11,6 +12,16 @@ app.use(methodOverride("_method"));
 
 app.get("/api/jobs", (req, res) => {
   Job.find()
+    .then(jobs => {
+      res.json(jobs);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.post("/api/jobs", (req, res) => {
+  Job.create(req.body)
     .then(jobs => {
       res.json(jobs);
     })
